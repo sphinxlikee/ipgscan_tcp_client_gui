8d8d2fcb9ed60ca15d7ab7cd7efeff0ba63cd038
Cem Polat <61560794+a2fStart@users.noreply.github.com>
2021-04-19 16:47:31 +0300

    Add riverpod state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:io';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'dart:developer' as developer;
 import 'package:flutter_tcp_client/tcp_client.dart';
 
 void main() {
@@ -21,123 +19,132 @@ class MyApp extends StatelessWidget {
   }
 }
 
-String serverAddress = '127.0.0.1';
-int serverPort = 64123;
-TCPClient tcpClient = TCPClient(serverAddress: serverAddress, serverPort: serverPort);
+TCPClient tcpClient;
+final serverAddressProvider = Provider<String>((ref) => '127.0.0.1');
+final serverPortProvider = Provider<int>((ref) => 64123);
+final tcpSocketProvider = ChangeNotifierProvider<TCPClient>(
+  (ref) {
+    tcpClient = TCPClient(
+        serverAddress: ref.read(serverAddressProvider),
+        serverPort: ref.read(serverPortProvider),
+        );
+    return tcpClient;
+  },
+);
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
+class MyHomePage extends StatelessWidget {
   final String title;
+  MyHomePage({this.title});
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('$title'),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          TCPDataSendButton(),
+          DataSendButton(),
           SizedBox(height: 10),
-          TCPDataSendIndicator(),
-          TCPDataReceiveIndicator(),
-          TCPConnectionIndicator(),
+          DataSendIndicator(),
+          DataReceiveIndicator(),
+          ConnectionIndicator(),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: tcpClient.isConnected
-            ? null
-            : () async {
-                await tcpClient.createConnection(tcpClient);
-                setState(() {});
-              },
-        tooltip: tcpClient.isConnected ? 'Connected' : 'Connect',
-        child: tcpClient.isConnected ? Icon(Icons.connect_without_contact_outlined) : Icon(Icons.touch_app_sharp),
-      ),
+      floatingActionButton: ConnectButton(),
     );
   }
 }
 
-class TCPDataSendButton extends StatefulWidget {
+class ConnectButton extends ConsumerWidget {
   @override
-  _TCPDataSendButtonState createState() => _TCPDataSendButtonState();
+  Widget build(BuildContext context, ScopedReader watch) {
+    final isConnected = watch(tcpSocketProvider).connectionState;
+    return FloatingActionButton(
+      onPressed: isConnected
+          ? null
+          : () async =>
+              await context.read(tcpSocketProvider).createConnection(tcpClient),
+      tooltip: isConnected ? 'Connected' : 'Connect',
+      child: isConnected
+          ? Icon(Icons.connect_without_contact_outlined)
+          : Icon(Icons.touch_app_sharp),
+    );
+  }
 }
 
-class _TCPDataSendButtonState extends State<TCPDataSendButton> {
+class ConnectionIndicator extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ScopedReader watch) {
+    final isConnected = watch(tcpSocketProvider).connectionState;
+    return ListTile(
+      leading: Container(
+        width: 30,
+        height: 30,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: isConnected ? Colors.green : Colors.red,
+        ),
+      ),
+      title: Text('Connection Status '),
+      subtitle: isConnected ? Text('Connected') : Text('Disconnected'),
+    );
+  }
+}
+
+class DataSendButton extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    final isConnected = watch(tcpSocketProvider).connectionState;
     return ElevatedButton(
-      onPressed: !tcpClient.isConnected
+      onPressed: !isConnected
           ? null
           : () {
-              tcpClient.socket.write('DateTime: ${DateTime.now()}\r\n');
-              setState(() {
-                tcpClient.dataSent = true;
-              });
+              context
+                  .read(tcpSocketProvider)
+                  .writeToStream(tcpClient, 'DateTime: ${DateTime.now()}');
             },
       child: Text('Send DateTime.now()'),
     );
   }
 }
 
-class TCPDataSendIndicator extends StatelessWidget {
+class DataSendIndicator extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ScopedReader watch) {
+    final isDataSent = watch(tcpSocketProvider).dataSentState;
     return ListTile(
       leading: Container(
         width: 30,
         height: 30,
         decoration: BoxDecoration(
           shape: BoxShape.circle,
-          color: tcpClient.dataSent ? Colors.green : Colors.red,
+          color: isDataSent ? Colors.green : Colors.red,
         ),
       ),
       title: Text('Data'),
-      subtitle: tcpClient.dataSent ? Text('Sent') : Text('Not sent'),
+      subtitle: isDataSent ? Text('Sent') : Text('Not sent'),
     );
   }
 }
 
-class TCPDataReceiveIndicator extends StatelessWidget {
+class DataReceiveIndicator extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ScopedReader watch) {
+    final isDataReceived = watch(tcpSocketProvider).dataReceivedState;
     return ListTile(
       leading: Container(
         width: 30,
         height: 30,
         decoration: BoxDecoration(
           shape: BoxShape.circle,
-          color: tcpClient.dataReceived ? Colors.green : Colors.red,
+          color: isDataReceived ? Colors.green : Colors.red,
         ),
       ),
       title: Text('Data'),
-      subtitle: tcpClient.dataReceived ? Text('Received') : Text('Not received'),
-    );
-  }
-}
-
-class TCPConnectionIndicator extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      leading: Container(
-        width: 30,
-        height: 30,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: tcpClient.isConnected ? Colors.green : Colors.red,
-        ),
-      ),
-      title: Text('Connection Status '),
-      subtitle: tcpClient.isConnected ? Text('Connected') : Text('Disconnected'),
+      subtitle: isDataReceived ? Text('Received') : Text('Not received'),
     );
   }
 }
--- lib/tcp_client.dart
+++ lib/tcp_client.dart
@@ -1,59 +1,98 @@
-import 'dart:math';
+import 'dart:async';
 
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'dart:io';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'dart:developer' as developer;
 
-class TCPClient {
+class TCPClient with ChangeNotifier {
   final String serverAddress;
   final int serverPort;
-  Socket socket;
-  bool isConnected, isDone, dataReceived, dataSent;
+  Socket _socket;
+  bool _isConnected, _dataReceived, _dataSent;
 
   TCPClient({
     @required this.serverAddress,
     @required this.serverPort,
-  })  : isConnected = false,
-        isDone = false,
-        dataReceived = false,
-        dataSent = false;
+  })  : _isConnected = false,
+        _dataReceived = false,
+        _dataSent = false;
 
-  Future<void> createConnection(TCPClient tc) async {
-    await Socket.connect(tc.serverAddress, tc.serverPort).catchError(
-      (e) {
-        print('connection has an error and socket is null.');
-        print(e);
+  get connectionState => _isConnected;
+  get dataReceivedState => _dataReceived;
+  get dataSentState => _dataSent;
+  get clientSocket => _socket;
+
+  void changeConnectionState() {
+    if (!_isConnected)
+      _isConnected = true;
+    else
+      _isConnected = false;
+
+    notifyListeners();
+  }
+
+  void changeDataReceivedState() {
+    _dataReceived = true;
+    notifyListeners();
+  }
+
+  void changeDataSentState() {
+    _dataSent = true;
+    notifyListeners();
+  }
+
+  void streamDone() async {
+    _dataReceived = false;
+    _dataSent = false;
+    await _socket.flush();
+    await _socket.close();
+    notifyListeners();
+  }
+
+  void writeToStream(TCPClient tcpClient, String data) {
+    tcpClient.clientSocket.write('$data\r\n');
+    if (!tcpClient.dataSentState) {
+      tcpClient.changeDataSentState();
+    }
+  }
+
+  Future<void> createConnection(TCPClient tcpc) async {
+    try {
+      tcpc._socket = await Socket.connect(tcpc.serverAddress, tcpc.serverPort);
+      tcpc.changeConnectionState();
+      developer.log(
+        'connected to ${tcpc._socket.address}:${tcpc._socket.port} from ${tcpc._socket.remoteAddress}:${tcpc._socket.remotePort}.',
+      );
+    } catch (e) {
+      print('connection has an error and socket is null.');
+      print(e);
+      return;
+    }
+
+    tcpc.clientSocket.listen(
+      (event) {
+        var received = String.fromCharCodes(event);
+        developer.log('received: $received');
+
+        if (!tcpc._dataReceived) {
+          tcpc.changeDataReceivedState();
+        }
       },
-    ).then(
-      (socket) {
-        tc.socket = socket;
-        tc.isDone = false;
-        tc.isConnected = true;
-        developer.log(
-          'connected to ${tc.socket.address}:${tc.socket.port} from ${tc.socket.remoteAddress}:${tc.socket.remotePort}.',
-        );
-
-        // use onData method in the code where you need.
-        tc.socket.listen(
-          (event) {
-            var received = String.fromCharCodes(event);
-            developer.log('received: $received');
-
-            if (!tc.dataReceived) {
-              tc.dataReceived = true;
-              tc.isDone = false;
-            }
-          },
-        )..onDone(
-            () {
-              tc.isDone = true;
-              tc.isConnected = false;
-              tc.dataReceived = false;
-              tc.dataSent = false;
-              developer.log('socket is closed');
-            },
-          );
-      },
-    );
+    )
+      ..onDone(
+        () {
+          tcpc.changeConnectionState();
+          tcpc.streamDone();
+          developer.log('socket is closed');
+        },
+      )
+      ..onError(
+        (error, stackTrace) {
+          print('$error');
+        },
+      );
   }
 }
+
+final firebaseAuthProvider = StreamProvider.autoDispose<Socket>((ref) {});
