7a03eb91f45dee243364835529e7bcf3c24c6f6d
Cem Polat <61560794+a2fStart@users.noreply.github.com>
2021-10-20 14:31:43 +0300

    update file

--- lib/provider/tcp_provider.dart
+++ lib/provider/tcp_provider.dart
@@ -1,6 +1,4 @@
 // ignore_for_file: avoid_print
-
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../tcp/tcp_client.dart';
 
@@ -12,26 +10,4 @@ final tcpClientProvider = ChangeNotifierProvider<TCPClient>(
     serverAddress: ref.read(serverAddressProvider).state,
     serverPort: ref.read(serverPortProvider).state,
   ),
-);
-
-class JobListNotifier extends ChangeNotifier {
-  String ipgJobs = '';
-  List<String> jobList = [];
-
-  void jobListParser(String jobListFromIPGScan) {
-    jobList = jobListFromIPGScan.split('\n');
-    print(jobList);
-    jobList.removeLast();
-    print(jobList.length);
-
-    notifyListeners();
-  }
-}
-
-final selectedJobIndexProvider = StateProvider<int>((ref) => 0);
-
-final jobListProvider = ChangeNotifierProvider<JobListNotifier>(
-  (ref) {
-    return JobListNotifier();
-  },
 );
