bbc0692a77e4af6f995a3d132d9fc57a1b0d9d32
Cem Polat <61560794+a2fStart@users.noreply.github.com>
2021-12-10 00:41:36 +0300

    delete unused providers for server addres and server port

--- lib/provider/tcp_provider.dart
+++ lib/provider/tcp_provider.dart
@@ -3,16 +3,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../tcp/tcp_client.dart';
 import '../ipg/ipgscan_api.dart';
 
-final serverAddressProvider = StateProvider<String>((ref) => '127.0.0.1');
-final serverPortProvider = StateProvider<int>((ref) => 88);
-
 TCPClient tcpClient = TCPClient.initial();
 
 final tcpClientProvider = StateNotifierProvider<TCPClientNotifier, TCPClient>(
   (ref) {
-    tcpClient.serverAddress = ref.read(serverAddressProvider);
     print('tcpClientProvider is here');
-    tcpClient.serverPort = ref.read(serverPortProvider);
     return TCPClientNotifier(tcpClient);
   },
 );
