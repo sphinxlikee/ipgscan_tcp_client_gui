ec4a14f7bf73f36f4b9fe080e7fb0b5ad355a4a0
Cem Polat <61560794+a2fStart@users.noreply.github.com>
2021-12-16 09:51:52 +0300

    update job list view to stateful widget and it works based on received data for job list

--- lib/widget/job_list_view.dart
+++ lib/widget/job_list_view.dart
@@ -1,53 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_tcp_client/ipg/ipgscan_api.dart';
 import '../provider/tcp_provider.dart';
 import '../provider/job_list_provider.dart';
 
-class ParseListButton extends ConsumerWidget {
-  const ParseListButton({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final isClientConnected = ref.watch(tcpClientProvider).isConnected;
-    final jobListWatcher = ref.watch(jobListProvider);
-    final tcpReceivedData = ref.watch(tcpClientProvider).receivedData;
-
-    return ElevatedButton(
-      onPressed: !isClientConnected
-          ? null
-          : () {
-              jobListWatcher.jobListParser(tcpReceivedData);
-            },
-      child: const Text('Parse the job list'),
-    );
-  }
-}
-
-class JobListView extends ConsumerWidget {
+class JobListView extends ConsumerStatefulWidget {
   const JobListView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final jobListWatcher = ref.watch(jobListProvider);
-    int selectedJobIndex = ref.watch(selectedJobIndexProvider);
+  ConsumerState<ConsumerStatefulWidget> createState() => _JobListViewState();
+}
 
-    return ListView.builder(
-      itemCount: jobListWatcher.jobList.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          leading: index == selectedJobIndex
-              ? const Icon(Icons.work, color: Colors.orange)
-              : const Icon(Icons.work_outline, color: Colors.black),
-          title: index == selectedJobIndex
-              ? Text(jobListWatcher.jobList[index],
-                  style: const TextStyle(
-                      color: Colors.orange, fontWeight: FontWeight.bold))
-              : Text(jobListWatcher.jobList[index],
-                  style: const TextStyle(color: Colors.black)),
-          onTap: () =>
-              ref.read(selectedJobIndexProvider.notifier).state = index,
-        );
+class _JobListViewState extends ConsumerState<JobListView> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final tcpClient = ref.watch(tcpClientProvider);
+    final jobListWatcher = ref.watch(jobListProvider);
+    final selectedJobIndex = ref.watch(selectedJobIndexProvider);
+    final lastCommand = ref.watch(lastCommandProvider);
+
+    WidgetsBinding.instance?.addPostFrameCallback(
+      (_) {
+        if (lastCommand == ipgScanCommandList.jobList) {
+          jobListWatcher.jobListParser(tcpClient.dataReceived);
+        }
       },
+    );
+
+    return Scrollbar(
+      controller: _scrollController,
+      child: ListView.builder(
+        controller: _scrollController,
+        itemCount: jobListWatcher.jobList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: index == selectedJobIndex
+                ? const Icon(Icons.work, color: Colors.orange)
+                : const Icon(Icons.work_outline, color: Colors.black),
+            title: index == selectedJobIndex
+                ? Text(
+                    jobListWatcher.jobList[index],
+                    style: const TextStyle(
+                        color: Colors.orange, fontWeight: FontWeight.bold),
+                  )
+                : Text(
+                    jobListWatcher.jobList[index],
+                    style: const TextStyle(color: Colors.black),
+                  ),
+            onTap: () =>
+                ref.read(selectedJobIndexProvider.notifier).state = index,
+          );
+        },
+      ),
     );
   }
 }
